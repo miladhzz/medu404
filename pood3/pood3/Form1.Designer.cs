b0b2b9e60b6850df9d564d2b7918a1b337e96445
miladhzz <miladhzz@gmail.com>
2026-02-11 20:34:17 +0330

    complete lock screen

---
+++ pood3/pood3/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace pood3
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.components = new System.ComponentModel.Container();
+      this.pictureBox3 = new System.Windows.Forms.PictureBox();
+      this.pictureBox2 = new System.Windows.Forms.PictureBox();
+      this.pictureBoxSignal = new System.Windows.Forms.PictureBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this.labelHour = new System.Windows.Forms.Label();
+      this.labelMinute = new System.Windows.Forms.Label();
+      this.labelDate = new System.Windows.Forms.Label();
+      this.label5 = new System.Windows.Forms.Label();
+      this.pictureBoxCamera = new System.Windows.Forms.PictureBox();
+      this.pictureBoxCall = new System.Windows.Forms.PictureBox();
+      this.timerSignal = new System.Windows.Forms.Timer(this.components);
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSignal)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCamera)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCall)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // pictureBox3
+      // 
+      this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+      this.pictureBox3.Image = global::pood3.Properties.Resources.Smartphone_Battery_High__Streamline_Nova;
+      this.pictureBox3.Location = new System.Drawing.Point(447, 4);
+      this.pictureBox3.Name = "pictureBox3";
+      this.pictureBox3.Size = new System.Drawing.Size(54, 30);
+      this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+      this.pictureBox3.TabIndex = 3;
+      this.pictureBox3.TabStop = false;
+      // 
+      // pictureBox2
+      // 
+      this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+      this.pictureBox2.Image = global::pood3.Properties.Resources._1;
+      this.pictureBox2.Location = new System.Drawing.Point(352, 4);
+      this.pictureBox2.Name = "pictureBox2";
+      this.pictureBox2.Size = new System.Drawing.Size(54, 30);
+      this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+      this.pictureBox2.TabIndex = 2;
+      this.pictureBox2.TabStop = false;
+      // 
+      // pictureBoxSignal
+      // 
+      this.pictureBoxSignal.BackColor = System.Drawing.Color.Transparent;
+      this.pictureBoxSignal.Image = global::pood3.Properties.Resources._3;
+      this.pictureBoxSignal.Location = new System.Drawing.Point(401, 4);
+      this.pictureBoxSignal.Name = "pictureBoxSignal";
+      this.pictureBoxSignal.Size = new System.Drawing.Size(54, 30);
+      this.pictureBoxSignal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+      this.pictureBoxSignal.TabIndex = 1;
+      this.pictureBoxSignal.TabStop = false;
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.BackColor = System.Drawing.Color.Transparent;
+      this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+      this.label1.Location = new System.Drawing.Point(12, 9);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(102, 25);
+      this.label1.TabIndex = 4;
+      this.label1.Text = "MiladCell";
+      // 
+      // labelHour
+      // 
+      this.labelHour.BackColor = System.Drawing.Color.Transparent;
+      this.labelHour.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+      this.labelHour.Location = new System.Drawing.Point(169, 126);
+      this.labelHour.Name = "labelHour";
+      this.labelHour.Size = new System.Drawing.Size(191, 104);
+      this.labelHour.TabIndex = 5;
+      this.labelHour.Text = "09";
+      this.labelHour.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+      // 
+      // labelMinute
+      // 
+      this.labelMinute.BackColor = System.Drawing.Color.Transparent;
+      this.labelMinute.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+      this.labelMinute.Location = new System.Drawing.Point(169, 230);
+      this.labelMinute.Name = "labelMinute";
+      this.labelMinute.Size = new System.Drawing.Size(191, 106);
+      this.labelMinute.TabIndex = 6;
+      this.labelMinute.Text = "33";
+      this.labelMinute.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+      // 
+      // labelDate
+      // 
+      this.labelDate.BackColor = System.Drawing.Color.Transparent;
+      this.labelDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+      this.labelDate.Location = new System.Drawing.Point(148, 328);
+      this.labelDate.Name = "labelDate";
+      this.labelDate.Size = new System.Drawing.Size(234, 56);
+      this.labelDate.TabIndex = 7;
+      this.labelDate.Text = "Fri, January 17";
+      this.labelDate.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+      // 
+      // label5
+      // 
+      this.label5.BackColor = System.Drawing.Color.Transparent;
+      this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+      this.label5.Location = new System.Drawing.Point(149, 681);
+      this.label5.Name = "label5";
+      this.label5.Size = new System.Drawing.Size(234, 56);
+      this.label5.TabIndex = 8;
+      this.label5.Text = "Swipe to open";
+      this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+      // 
+      // pictureBoxCamera
+      // 
+      this.pictureBoxCamera.BackColor = System.Drawing.Color.Transparent;
+      this.pictureBoxCamera.Image = global::pood3.Properties.Resources.Camera_Circle__Streamline_Nova;
+      this.pictureBoxCamera.Location = new System.Drawing.Point(418, 776);
+      this.pictureBoxCamera.Name = "pictureBoxCamera";
+      this.pictureBoxCamera.Size = new System.Drawing.Size(70, 66);
+      this.pictureBoxCamera.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+      this.pictureBoxCamera.TabIndex = 9;
+      this.pictureBoxCamera.TabStop = false;
+      // 
+      // pictureBoxCall
+      // 
+      this.pictureBoxCall.BackColor = System.Drawing.Color.Transparent;
+      this.pictureBoxCall.Image = global::pood3.Properties.Resources.Phone_Circle__Streamline_Nova;
+      this.pictureBoxCall.Location = new System.Drawing.Point(31, 776);
+      this.pictureBoxCall.Name = "pictureBoxCall";
+      this.pictureBoxCall.Size = new System.Drawing.Size(70, 66);
+      this.pictureBoxCall.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+      this.pictureBoxCall.TabIndex = 10;
+      this.pictureBoxCall.TabStop = false;
+      // 
+      // timerSignal
+      // 
+      this.timerSignal.Enabled = true;
+      this.timerSignal.Interval = 1000;
+      this.timerSignal.Tick += new System.EventHandler(this.timerSignal_Tick);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BackgroundImage = global::pood3.Properties.Resources.saa16;
+      this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+      this.ClientSize = new System.Drawing.Size(521, 887);
+      this.Controls.Add(this.pictureBoxCall);
+      this.Controls.Add(this.pictureBoxCamera);
+      this.Controls.Add(this.label5);
+      this.Controls.Add(this.labelDate);
+      this.Controls.Add(this.labelMinute);
+      this.Controls.Add(this.labelHour);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.pictureBox3);
+      this.Controls.Add(this.pictureBox2);
+      this.Controls.Add(this.pictureBoxSignal);
+      this.DoubleBuffered = true;
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+      this.Name = "Form1";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "Samsung";
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSignal)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCamera)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCall)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+    private System.Windows.Forms.PictureBox pictureBoxSignal;
+    private System.Windows.Forms.PictureBox pictureBox2;
+    private System.Windows.Forms.PictureBox pictureBox3;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label labelHour;
+    private System.Windows.Forms.Label labelMinute;
+    private System.Windows.Forms.Label labelDate;
+    private System.Windows.Forms.Label label5;
+    private System.Windows.Forms.PictureBox pictureBoxCamera;
+    private System.Windows.Forms.PictureBox pictureBoxCall;
+    private System.Windows.Forms.Timer timerSignal;
+  }
+}
+
---
+++ pood3/pood3/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pood3
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+
+      pictureBoxCamera.MouseDown += pictureBox1_MouseDown;
+      pictureBoxCamera.MouseMove += pictureBox1_MouseMove;
+      pictureBoxCamera.MouseUp += pictureBox1_MouseUp; 
+      
+      pictureBoxCall.MouseDown += pictureBoxCall_MouseDown;
+      pictureBoxCall.MouseMove += pictureBoxCall_MouseMove;
+      pictureBoxCall.MouseUp += pictureBoxCall_MouseUp;
+
+      startPoint = pictureBoxCamera.Location;
+      startPointCall = pictureBoxCall.Location;
+    }
+
+    private bool isDragging = false;
+    private bool isDraggingCall = false;
+    private Point startPoint = new Point(0, 0);
+    private Point startPointCall = new Point(0, 0);
+
+
+    private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+    {
+      if (isDragging)
+      {
+        pictureBoxCamera.Left += e.X;
+        pictureBoxCamera.Top += e.Y;
+
+      }
+    }
+
+    private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+    {
+      if (e.Button == MouseButtons.Left)
+      {
+        isDragging = false;
+        pictureBoxCamera.Location = startPoint;
+      }
+    }
+
+    private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+    {
+      if (e.Button == MouseButtons.Left)
+      {
+        isDragging = true;
+      }
+
+
+    }
+      private void pictureBoxCall_MouseMove(object sender, MouseEventArgs e)
+    {
+      if (isDraggingCall)
+      {
+        pictureBoxCall.Left += e.X;
+        pictureBoxCall.Top += e.Y;
+
+      }
+    }
+
+    private void pictureBoxCall_MouseUp(object sender, MouseEventArgs e)
+    {
+      if (e.Button == MouseButtons.Left)
+      {
+        isDraggingCall = false;
+        pictureBoxCall.Location = startPointCall;
+      }
+    }
+
+    private void pictureBoxCall_MouseDown(object sender, MouseEventArgs e)
+    {
+      if (e.Button == MouseButtons.Left)
+      {
+        isDraggingCall = true;
+      }
+    }
+
+    private void timerSignal_Tick(object sender, EventArgs e)
+    {
+      Random random = new Random();
+      int i = random.Next(1, 4);
+      pictureBoxSignal.ImageLocation = "img\\signal\\" + i.ToString() + ".png";
+
+
+      labelHour.Text = DateTime.Now.ToString("HH");
+      labelMinute.Text = DateTime.Now.ToString("mm");
+
+      labelDate.Text = DateTime.Now.ToString("ddd, MMMM dd");
+    }
+  }
+}
